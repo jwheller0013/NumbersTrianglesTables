db471fcfd3b344ccd3383b382e6487f6868f36e7
jwheller0013 <jwheller0013@yahoo.com>
2025-03-03 17:12:32 -0500

    comments for Table

--- src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/TableUtilities.java
@@ -2,14 +2,22 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class TableUtilities {
     public static String getMultiplicationTable(int tableSize) {
+
         return null;
     }
 
     public static String getSmallMultiplicationTable() {
-        return null;
+
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+
+        return getMultiplicationTable(10);
     }
 }
+// format code that has 3 spaces (three characters wide) and "\n"
+// create a next line to use
+// what do you start by and what do you step by
+// try a for loop of 1 to 5, 2 to 10 by 2s, 3 to 15 by 3s
+// make it a StringBuilder
